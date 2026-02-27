405c36fe4afc92e44a6614c3ab86ddf501cd2ccb
Jennifer <jennifersilvapt@gmail.com>
2026-02-27 20:41:57 +0000

    Add cod app

---
+++ app2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('notebooks/vehicles_us.csv')
+
+st.header("Análise de Dados de Carros")
+
+hist_button = st.button('Criar histograma')
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if st.button('Criar gráfico de dispersão'):
+    st.write('Criando gráfico de dispersão: Quilometragem vs Preço')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
